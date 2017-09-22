ff61cdcca655b3c7f23e40c110b0062fddb4db2a
yhq <854968725@qq.com>
2017-09-22 09:12:24 +0800

    another stable vision

---
+++ app/src/main/java/com/example/administrator/exmusic_final/StepView.java
@@ -0,0 +1,18 @@
+package com.example.administrator.exmusic_final;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2017/9/22.
+ */
+
+public class StepView extends AppCompatActivity{
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+
+    }
+}
